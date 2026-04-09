7c89d27bfc9fbcf9ef83347e766f87796a9ac587
OlekR123 <oleg.rumzik@yandex.ru>
2026-04-09 15:15:57 +0300

    new mailer

--- api/server.js
+++ api/server.js
@@ -36,7 +36,9 @@ app.use(express.json({ limit: '10mb' }));
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://winpack.onrender.com'
+    'https://winpack.onrender.com',
+    'https://winpack.site',
+    'https://www.winpack.site'
 ];
 
 app.use('/api', cors({
